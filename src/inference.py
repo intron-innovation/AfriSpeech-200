93a0b94c40c5b05b30e4accb5bd57c6743c7e56e
Tejumade Afonja <tejumadeafonja@Tejumades-MBP-2.local>
2022-11-26 15:08:54 +0100

    code refactor;
    - enable loading arbitrary audio len but fails of length exceeds MAX_MODEL_AUDIO_LEN
    - capitalize all the global variables so they easy to find
    - add todo for implementing long audio processing logic
    
     Please enter the commit message for your changes. Lines starting

--- src/inference.py
+++ src/inference.py
@@ -23,20 +23,42 @@ set_seed(1778)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 wer_metric = load_metric("wer")
 
-model = None
-processor = None
-sampling_rate = 16000
+MODEL = None
+PROCESSOR = None
+SAMPLING_RATE = 16000
+AUDIO_DIR = "./data/"
+LONG_SPEECH_DETECTED = False
+MAX_MODEL_AUDIO_LEN = 59
 
 
-def load_data(data_path, max_audio_len_secs=17):
+def load_data(data_path, max_audio_len_secs=MAX_MODEL_AUDIO_LEN):
     """
     load train/dev/test data from csv path.
     :param max_audio_len_secs: int
     :param data_path: str
     :return: Dataset instance
     """
+    global LONG_SPEECH_DETECTED
+
     data = pd.read_csv(data_path)
-    data = data[data.duration < max_audio_len_secs]
+    data["audio_paths"] = data["audio_paths"].apply(
+        lambda x: x.replace("/AfriSpeech-100/", AUDIO_DIR)
+    )
+
+    if max_audio_len_secs is not None:
+        data = data[data.duration < max_audio_len_secs]
+
+    else:
+        # Check if any of the sample is longer than
+        # the specified MAX_MODEL_AUDIO_LEN
+        if (data.duration.to_numpy() > MAX_MODEL_AUDIO_LEN).any():
+            LONG_SPEECH_DETECTED = True
+            print(
+                f"Detected speech longer than {MAX_MODEL_AUDIO_LEN} "
+                "-- such speech length cannot be processed, even on cpu!"
+                "\nSet `max_audio_len_secs` to filter longer speech!"
+            )
+
     data["text"] = data["transcript"]
     print("before dedup", data.shape)
     data.drop_duplicates(subset=["audio_paths"], inplace=True)
@@ -50,23 +72,22 @@ def compute_benchmarks(batch):
     :param batch:
     :return:
     """
-    speech, fs = librosa.load(batch["audio_paths"], sr=sampling_rate)
-    if fs != sampling_rate:
-        speech = librosa.resample(speech, fs, sampling_rate)
+    speech, fs = librosa.load(batch["audio_paths"], sr=SAMPLING_RATE)
+    if fs != SAMPLING_RATE:
+        speech = librosa.resample(speech, fs, SAMPLING_RATE)
 
-    input_features = processor(
-        speech, sampling_rate=sampling_rate, padding=True, return_tensors="pt"
+    input_features = PROCESSOR(
+        speech, sampling_rate=SAMPLING_RATE, padding=True, return_tensors="pt"
     )
     input_val = input_features.input_values.to(device)
-
     with torch.no_grad():
-        logits = model(input_val).logits
+        logits = MODEL(input_val).logits
         batch["logits"] = logits
 
     pred_ids = torch.argmax(torch.tensor(batch["logits"]), dim=-1)
-    pred = processor.batch_decode(pred_ids)[0]
+    pred = PROCESSOR.batch_decode(pred_ids)[0]
     batch["predictions"] = cleanup(pred)
-    batch["reference"] = cleanup(batch["transcript"]).lower()
+    batch["reference"] = cleanup(batch["text"]).lower()
     batch["wer"] = wer_metric.compute(
         predictions=[batch["predictions"]], references=[batch["reference"]]
     )
@@ -83,7 +104,7 @@ def write_pred(model_id_or_path, results, wer, output_dir="./results"):
     :return: DataFrame
     """
     model_id_or_path = model_id_or_path.replace("/", "-")
-    cols = ["audio_paths", "transcript", "reference", "predictions", "wer", "accent"]
+    cols = ["audio_paths", "text", "reference", "predictions", "wer", "accent"]
     predictions_data = {col: results[col] for col in cols}
     predictions_df = pd.DataFrame(data=predictions_data)
 
@@ -100,25 +121,37 @@ def run_benchmarks(model_id_or_path, test_dataset, output_dir="./results"):
     :param test_dataset: Dataset
     :return:
     """
-    global model, processor
+    global MODEL, PROCESSOR
     tsince = int(round(time.time()))
     n_samples = len(test_dataset)
-    processor = Wav2Vec2Processor.from_pretrained(model_id_or_path)
+    PROCESSOR = Wav2Vec2Processor.from_pretrained(model_id_or_path)
     if "hubert" in model_id_or_path:
-        model = HubertForCTC.from_pretrained(model_id_or_path).to(device)
+        MODEL = HubertForCTC.from_pretrained(model_id_or_path).to(device)
     else:
-        model = Wav2Vec2ForCTC.from_pretrained(model_id_or_path).to(device)
-    test_dataset = test_dataset.map(compute_benchmarks)
-    all_wer = wer_metric.compute(
-        predictions=test_dataset["predictions"], references=test_dataset["reference"]
-    )
-    print(f"all_wer: {all_wer:0.03f}")
-    write_pred(model_id_or_path, test_dataset, all_wer, output_dir=output_dir)
-    ttime_elapsed = int(round(time.time())) - tsince
-    print(
-        f"{model_id_or_path}-- Inference Time: {ttime_elapsed / 60:.4f}m | "
-        f"{ttime_elapsed / n_samples:.4f}s per sample"
-    )
+        MODEL = Wav2Vec2ForCTC.from_pretrained(model_id_or_path).to(device)
+
+    if LONG_SPEECH_DETECTED:
+        TODO: str("Write function to handle long speech!")
+        raise NotImplementedError(
+            "Long speech detected when loading the audio paths, "
+            "there is currently no logic to handle long speech, "
+            "set the `max_audio_len_secs` to <59 secs!"
+        )
+
+    else:
+        test_dataset = test_dataset.map(compute_benchmarks)
+
+        all_wer = wer_metric.compute(
+            predictions=test_dataset["predictions"],
+            references=test_dataset["reference"],
+        )
+        print(f"all_wer: {all_wer:0.03f}")
+        write_pred(model_id_or_path, test_dataset, all_wer, output_dir=output_dir)
+        ttime_elapsed = int(round(time.time())) - tsince
+        print(
+            f"{model_id_or_path}-- Inference Time: {ttime_elapsed / 60:.4f}m | "
+            f"{ttime_elapsed / n_samples:.4f}s per sample"
+        )
 
 
 def parse_argument():
@@ -150,22 +183,8 @@ if __name__ == "__main__":
     # Make output directory if does not already exist.
     os.makedirs(args.output_dir, exist_ok=True)
 
-    # Load dataset using huggyface dataset class because the
-    # `compute_benchmarks` function is mapped in the `run_benchmarks`
-    # function. So, `test_dataset` must be a Dataset type.
-    ds = load_dataset("csv", data_files={"dev": args.data_csv_path})["dev"]
-
-    # This block can be removed once the data csv file removes the
-    # `/AfriSpeech-100` prepended to the audio path.
-    DATA_PAR_DIR = os.path.dirname(args.data_csv_path)
-
-    def _fix_audio_path(batch):
-        batch["audio_paths"] = batch["audio_paths"].replace(
-            "/AfriSpeech-100", f"{DATA_PAR_DIR}"
-        )
-        return batch
-
-    ds = ds.map(_fix_audio_path).select(range(10))
+    # Load data.
+    ds = load_data(args.data_csv_path, max_audio_len_secs=None).select(range(2))
 
     run_benchmarks(
         model_id_or_path=args.model_id_or_path,
