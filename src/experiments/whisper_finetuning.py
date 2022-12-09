490cf54541ac4cc3f1435ead2230d179dcea95c2
Tejumade Afonja <tejumadeafonja@Tejumades-MacBook-Pro-2.local>
2022-12-09 18:51:07 +0100

    modifiy training script for whisper

--- src/experiments/whisper_finetuning.py
+++ src/experiments/whisper_finetuning.py
@@ -1,10 +1,9 @@
 import os
 import argparse
 import pandas as pd
-import time
-from datasets import load_dataset, load_metric, Dataset, Audio
-import evaluate
-import librosa
+from pathlib import Path
+from functools import partial
+from datasets import load_metric, Dataset, Audio
 import torch
 from transformers import (
     WhisperForConditionalGeneration,
@@ -13,12 +12,13 @@ from transformers import (
     WhisperTokenizer,
     set_seed,
     Seq2SeqTrainingArguments,
-    Seq2SeqTrainer
+    Seq2SeqTrainer,
 )
+import librosa
 from dataclasses import dataclass
 from typing import Any, Dict, List, Union
-from src.utils import cleanup
-
+from src.utils.utils import cleanup
+from src.inference.inference import write_pred
 
 set_seed(1778)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -26,40 +26,19 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def parse_argument():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--data_csv_path",
-        type=str,
-        default="./data/intron-dev-public-3232.csv",
-        help="path to data csv file",
-    )
-    parser.add_argument(
-        "--audio_dir",
-        type=str,
-        default="./data/",
-        help="directory to locate the audio",
-    )
-    parser.add_argument(
-        "--model_id_or_path",
-        type=str,
-        default="openai/whisper-medium",
-        help="id of the model or path to huggyface model",
-    )
-    parser.add_argument(
-        "--output_dir", type=str, default="./results", help="directory to store results"
-    )
-    parser.add_argument(
-        "--max_audio_len_secs",
-        type=int,
-        default=17,
-        help="maximum audio length passed to the inference model should",
-    )
 
-    parser.add_argument(
-        "--sampling_rate",
-        type=int,
-        default=16000,
-        help="sampling rate of audio",
-    )
+    # fmt: off
+    parser.add_argument("--audio_dir", type=str, default="./data/", help="directory to locate the audio")
+    parser.add_argument("--model_id_or_path", type=str, default="openai/whisper-medium", 
+                        help="id of the model or path to huggyface model")
+    parser.add_argument("--output_dir", type=str, default="./whisper-medium-finetune", 
+                        help="directory to store results")
+    parser.add_argument("--train", action='store_true', help="Whether or not to train the model")
+    parser.add_argument("--evaluate", action='store_true', help="Whether or not to evaluate the trained the model")
+    parser.add_argument("--max_audio_len_secs", type=int, default=17,
+                        help="maximum audio length passed to the inference model should")
+    parser.add_argument("--sampling_rate", type=int, default=16000, help="sampling rate of audio")
+    # fmt: on
 
     args = parser.parse_args()
     return args
@@ -81,7 +60,7 @@ def load_data(
     :param data_path: str
     :return: Dataset instance
     """
-    data = pd.read_csv(data_path)
+    data = pd.read_csv(audio_dir + data_path)
     data["audio_paths"] = data["audio_paths"].apply(
         lambda x: x.replace("/AfriSpeech-100/", audio_dir)
     )
@@ -109,18 +88,17 @@ def load_data(
     return Dataset.from_pandas(data)
 
 
-def transcribe_whisper(batch):
+def transcribe_whisper(batch, processor, model, metric, sampling_rate, device):
     """
     Run inference on batch using whisper model
     :param batch:
     :return:
     """
 
-    speech = batch["audio_paths"]["array"]
-    sampling_rate = batch["audio_paths"]["sampling_rate"]
-    input_features = processor(speech, sampling_rate=sampling_rate, return_tensors="pt")
-    input_features = input_features.input_features.to(device)
-
+    speech, sampling_rate = librosa.load(batch["audio_paths"], sr=sampling_rate)
+    input_features = processor(
+        speech, sampling_rate=sampling_rate, return_tensors="pt"
+    ).input_features.to(device)
     generated_ids = model.generate(inputs=input_features)
     predicted_transcript = processor.batch_decode(
         generated_ids, skip_special_tokens=True
@@ -138,7 +116,7 @@ def transcribe_whisper(batch):
     return batch
 
 
-def prepare_dataset(batch):
+def prepare_dataset(batch, feature_extractor, tokenizer):
     # Load and resample audio data to 16KHz
     audio = batch["audio_paths"]
 
@@ -160,13 +138,13 @@ class DataCollatorSpeechSeq2SeqWithPadding:
     def __call__(
         self, features: List[Dict[str, Union[List[int], torch.Tensor]]]
     ) -> Dict[str, torch.Tensor]:
-    
+
         # Split inputs and labels since they have to be of different lengths and need different padding methods
         # First treat the audio inputs by simply returning torch tensors
         input_features = [
             {"input_features": feature["input_features"]} for feature in features
         ]
-        
+
         batch = self.processor.feature_extractor.pad(
             input_features, return_tensors="pt"
         )
@@ -176,12 +154,12 @@ class DataCollatorSpeechSeq2SeqWithPadding:
 
         # Pad the labels to max length
         labels_batch = self.processor.tokenizer.pad(label_features, return_tensors="pt")
-        
+
         # Replace padding wit -100 to ignore loss correctly
         labels = labels_batch["input_ids"].masked_fill(
             labels_batch.attention_mask.ne(1), -100
         )
-        
+
         # If bos token is appended in previous tokenization step,
         # cut bos token here as it is appended later anyways
         if (labels[:, 0] == self.processor.tokenizer.bos_token_id).all().cpu().item():
@@ -192,7 +170,7 @@ class DataCollatorSpeechSeq2SeqWithPadding:
         return batch
 
 
-def compute_metrics(pred):
+def compute_metrics(pred, metric):
     pred_ids = pred.predictions
     label_ids = pred.label_ids
 
@@ -212,105 +190,146 @@ if __name__ == "__main__":
     """Run main script"""
     args = parse_argument()
 
-    # Make output directory if does not already exist
-    os.makedirs(args.output_dir, exist_ok=True)
+    if args.train:
+        # Make output directory if does not already exist
+        os.makedirs(args.output_dir, exist_ok=True)
 
-    # Define processor, feature extractor, tokenizer and model
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    processor = WhisperProcessor.from_pretrained(
-        args.model_id_or_path, language="en", task="transcribe"
-    )
-    feature_extractor = WhisperFeatureExtractor.from_pretrained(args.model_id_or_path)
-    tokenizer = WhisperTokenizer.from_pretrained(
-        args.model_id_or_path, language="en", task="transcribe"
-    )
-    model = WhisperForConditionalGeneration.from_pretrained(args.model_id_or_path)
+        # Load the dataset
+        # fmt: off
+        train_dataset = load_data(
+            data_path="intron-train-public-58001.csv",
+            audio_dir=args.audio_dir, 
+            duration=args.max_audio_len_secs
+        )       
+        dev_dataset = load_data(
+            data_path="intron-dev-public-3232.csv",
+            audio_dir=args.audio_dir,
+            duration=args.max_audio_len_secs
+        )
 
-    # Override generation arguments
-    model.config.forced_decoder_ids = None
-    model.config.suppress_tokens = []
+        # Process the audio
+        train_dataset = train_dataset.cast_column("audio_paths", Audio(sampling_rate=args.sampling_rate))
+        dev_dataset = dev_dataset.cast_column("audio_paths", Audio(sampling_rate=args.sampling_rate))
 
-    # Load the dataset
-    afrispeech = load_data(
-        data_path=args.data_csv_path,
-        audio_dir=args.audio_dir,
-        accent="yoruba",
-        age_group="19-25",
-        domain="clinical",
-    )
+        # Define processor, feature extractor, tokenizer and model
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        processor = WhisperProcessor.from_pretrained(args.model_id_or_path, language="en", task="transcribe")
+        feature_extractor = WhisperFeatureExtractor.from_pretrained(args.model_id_or_path)
+        tokenizer = WhisperTokenizer.from_pretrained(args.model_id_or_path, language="en", task="transcribe")
+        model = WhisperForConditionalGeneration.from_pretrained(args.model_id_or_path)
+        
+        # Prepare dataset for training
+        prepare_dataset = partial(prepare_dataset, feature_extractor=feature_extractor, tokenizer=tokenizer)
+        train_dataset = train_dataset.map(prepare_dataset, remove_columns=train_dataset.column_names)
+        dev_dataset = dev_dataset.map(prepare_dataset, remove_columns=dev_dataset.column_names)
+        print(train_dataset, dev_dataset)
+        # fmt: on
 
-    afrispeech = afrispeech.remove_columns(
-        [
-            "idx",
-            "user_ids",
-            "age_group",
-            "accent",
-            "country",
-            "nchars",
-            "audio_ids",
-            "duration",
-            "origin",
-            "domain",
-            "split",
-            "transcript",
+        # Override generation arguments
+        model.config.forced_decoder_ids = None
+        model.config.suppress_tokens = []
+
+        # Instantiate data collator
+        data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
+
+        # Load metric
+        metric = load_metric("wer")
+        compute_metrics = partial(compute_metrics, metric=metric)
+
+        # Define the training configuration
+        training_args = Seq2SeqTrainingArguments(
+            output_dir=args.output_dir,
+            per_device_train_batch_size=5,
+            gradient_accumulation_steps=4,
+            learning_rate=1e-4,
+            warmup_steps=500,
+            num_train_epochs=5,
+            gradient_checkpointing=True,
+            fp16=True,
+            evaluation_strategy="steps",
+            per_device_eval_batch_size=8,
+            predict_with_generate=True,
+            generation_max_length=25,
+            save_steps=100,
+            eval_steps=100,
+            logging_steps=25,
+            report_to="tensorboard",
+            load_best_model_at_end=True,
+            greater_is_better=False,
+            push_to_hub=False,
+        )
+
+        # # Define the trainer
+        trainer = Seq2SeqTrainer(
+            args=training_args,
+            model=model,
+            train_dataset=train_dataset,
+            eval_dataset=train_dataset,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
+            tokenizer=processor.feature_extractor,
+        )
+
+        # Save the processor object once before starting to train
+        processor.save_pretrained(training_args.output_dir)
+        args.model_id_or_path = Path(args.output_dir+"/checkpoint-5")
+
+        # Train the model
+        trainer.train()
+
+    if args.evaluate:
+        args.model_id_or_path = Path(args.model_id_or_path)
+
+        # Make output directory if does not already exist
+        os.makedirs(args.output_dir, exist_ok=True)
+
+        # Define processor, feature extractor, tokenizer and model
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        # Load dev dataset
+        dev_dataset = load_data(
+            data_path="intron-dev-public-3232.csv",
+            audio_dir=args.audio_dir,
+            duration=args.max_audio_len_secs,
+        )
+
+        # Define processor and model
+        processor = WhisperProcessor.from_pretrained(args.model_id_or_path.parents[0], language="en", task="transcribe")
+        model = WhisperForConditionalGeneration.from_pretrained(args.model_id_or_path)
+
+        # Define metric
+        metric = load_metric("wer")
+
+        # Run inference
+        transcribe_whisper = partial(
+            transcribe_whisper,
+            processor=processor,
+            model=model,
+            metric=metric,
+            sampling_rate=args.sampling_rate,
+            device=device,
+        )
+        dev_dataset = dev_dataset.map(transcribe_whisper)
+
+        all_wer = metric.compute(
+            predictions=dev_dataset["predictions"],
+            references=dev_dataset["text"],
+        )
+
+        print(f"all_wer: {all_wer:0.03f}")
+
+        # fmt: off
+        output_cols = [
+            "audio_paths", "accent", "text", "predictions", 
+            "clean_wer", "clean_text", "clean_predictions", "wer"
         ]
-    )
+        # fmt: on
 
-    # Process the audio
-    afrispeech = afrispeech.cast_column("audio_paths", Audio(sampling_rate=16000))
-    
-    # Train-test split
-    afrispeech = afrispeech.train_test_split(test_size=0.2, shuffle=True, seed=42)
-    
-    afrispeech = afrispeech.map(prepare_dataset, remove_columns=afrispeech.column_names["train"])
-    print(afrispeech)
-    
-    # Load metric
-    metric = load_metric("wer")
-
-    # Instantiate data collator
-    data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=processor)
-    
-    # dc = data_collator(afrispeech)
-    
-    # print(dc)
-
-    # Define the training configuration
-    training_args = Seq2SeqTrainingArguments(
-        output_dir="./whisper-medium-afrispeech_clinical",
-        per_device_train_batch_size=5,
-        gradient_accumulation_steps=1,
-        learning_rate=1e-5,
-        warmup_steps=500,
-        max_steps=500,
-        gradient_checkpointing=True,
-        fp16=False,
-        evaluation_strategy="steps",
-        per_device_eval_batch_size=8,
-        predict_with_generate=True,
-        generation_max_length=25,
-        save_steps=100,
-        eval_steps=100,
-        logging_steps=25,
-        report_to="tensorboard",
-        load_best_model_at_end=True,
-        greater_is_better=False,
-        push_to_hub=False
-    )
-    
-    # # Define the trainer
-    trainer = Seq2SeqTrainer(
-        args=training_args,
-        model=model,
-        train_dataset=afrispeech["train"],
-        eval_dataset=afrispeech["test"],
-        data_collator=data_collator,
-        compute_metrics=compute_metrics,
-        tokenizer=processor.feature_extractor
-    )
-    
-    # Save the processor object once before starting to train
-    processor.save_pretrained(training_args.output_dir)
-    
-    # Train the model
-    trainer.train()
+        # Write prediction to output folder
+        write_pred(
+            str(args.model_id_or_path),
+            dev_dataset,
+            all_wer,
+            cols=output_cols,
+            output_dir=args.output_dir,
+        )
