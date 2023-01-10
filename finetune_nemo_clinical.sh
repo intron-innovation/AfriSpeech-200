c844ca740b1e06ea2436b11cf78785b831f955b1
chrisemezue <chris.emezue@gmail.com>
2023-01-10 02:54:37 -0500

    sbatch files for training

---
+++ finetune_nemo_clinical.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=finetune_whisper
+#SBATCH --gres=gpu:rtx8000:1
+#SBATCH --mem=180G
+#SBATCH --time=72:00:00
+#SBATCH --partition=long
+#SBATCH --error=/home/mila/c/chris.emezue/AfriSpeech-Dataset-Paper/slurmerror_nemo_clinical_%j.txt
+#SBATCH --output=/home/mila/c/chris.emezue/AfriSpeech-Dataset-Paper/slurmoutput_nemo_clinical_%j.txt
+
+cd /home/mila/c/chris.emezue/AfriSpeech-Dataset-Paper
+module load anaconda/3
+#module load cuda/11.0/cudnn/8.0
+conda activate /home/mila/c/chris.emezue/scratch/nemo2
+export LD_LIBRARY_PATH=/home/mila/c/chris.emezue/scratch/nemo2/lib/python3.8/site-packages/nvidia/cublas/lib/:$LD_LIBRARY_PATH
+
+
+python3 src/train/nemo_ctc_finetuning.py --config src/config/nemo_clinical.ini
