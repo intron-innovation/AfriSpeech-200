a270dc9365631c1baad3f47ea26bdfc43edb7c95
Owos <owodunniabraham@gmail.com>
2023-02-27 09:38:39 +0200

    add training and prep data fxn to Introtrainer

--- src/utils/sampler.py
+++ src/utils/sampler.py
@@ -348,6 +348,70 @@ class IntronTrainer(Trainer):
     def __init__(self, sampler=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.sampler = sampler
+
+
+    def _prepare_inputs(self, inputs: Dict[str, Union[torch.Tensor, Any]]) -> Dict[str, Union[torch.Tensor, Any]]:
+        for k, v in inputs.items():
+            if isinstance(v, torch.Tensor):
+                kwargs = dict(device=self.args.device)
+                if self.deepspeed and inputs[k].dtype != torch.int64:
+                    kwargs.update(dict(dtype=self.args.hf_deepspeed_config.dtype()))
+                inputs[k] = v.to(**kwargs)
+
+            if k == 'labels': # labels are list of tensor, not tensor, special handle here
+                for i in range(len(inputs[k])):
+                    kwargs = dict(device=self.args.device)
+                    if self.deepspeed and inputs[k][i].dtype != torch.int64:
+                        kwargs.update(dict(dtype=self.args.hf_deepspeed_config.dtype()))
+                    inputs[k][i] = inputs[k][i].to(**kwargs)
+
+        if self.args.past_index >= 0 and self._past is not None:
+            inputs["mems"] = self._past
+
+        return inputs
+
+    def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
+        """
+        Perform a training step on a batch of inputs.
+        Subclass and override to inject custom behavior.
+        Args:
+            model (:obj:`nn.Module`):
+                The model to train.
+            inputs (:obj:`Dict[str, Union[torch.Tensor, Any]]`):
+                The inputs and targets of the model.
+                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+                argument :obj:`labels`. Check your model's documentation for all accepted arguments.
+        Return:
+            :obj:`torch.Tensor`: The tensor with training loss on this batch.
+        """
+
+        model.train()
+        inputs = self._prepare_inputs(inputs)
+
+        if self.use_amp:
+            with autocast():
+                loss = self.compute_loss(model, inputs)
+        else:
+            loss = self.compute_loss(model, inputs)
+
+        if self.args.n_gpu > 1:
+            loss = loss.mean()
+
+        if self.args.gradient_accumulation_steps > 1:
+            loss = loss / self.args.gradient_accumulation_steps
+
+        if self.use_amp:
+            self.scaler.scale(loss).backward()
+        elif self.use_apex:
+            with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                scaled_loss.backward()
+        elif self.deepspeed:
+            self.deepspeed.backward(loss)
+        else:
+            loss.backward()
+
+        return loss.detach()
+    
     
     def _get_eval_sampler(self, eval_dataset: Dataset) -> Optional[torch.utils.data.Sampler]:
         # Deprecated code
