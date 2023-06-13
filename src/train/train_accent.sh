9e5bf9d9862c67082074919ad4d1d50dfa9c44a9
chrisemezue <chris.emezue@gmail.com>
2023-06-13 17:24:13 -0400

    updates from chris 13th june

--- src/train/train_accent.sh
+++ src/train/train_accent.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+cd /home/mila/c/chris.emezue/AfriSpeech-Dataset-Paper
+module load anaconda/3
+#module load cuda/11.0/cudnn/8.0
+conda activate /home/mila/c/chris.emezue/scratch/nemo2
+export PYTHONPATH=$PYTHONPATH:/home/mila/c/chris.emezue/AfriSpeech-Dataset-Paper
 
 python3 -m src.train.train -c src/config/accent_fold/config_xlsr_accentfold.ini
